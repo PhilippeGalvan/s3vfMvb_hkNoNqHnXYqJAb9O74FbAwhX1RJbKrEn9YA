025bc2e272223d6538bd0a2fc457769f9df23ddc
Philippe Galvan <git.philippeG@outlook.com>
2020-08-25 13:59:05 +0200

    Add basic docstrings

--- senndertest/senndermovies/processing.py
+++ senndertest/senndermovies/processing.py
@@ -1,6 +1,12 @@
 import requests
 
 def get_movies_with_id():
+    """
+    Get all the movies from the ghibli API.
+    It returns a dictionnary containing all films name indexed by id
+    if the API returns a valid result. Otherwise it returns an empty dict.
+    """
+
     response = requests.get(
         'https://ghibliapi.herokuapp.com/films'
     )
@@ -9,6 +15,12 @@ def get_movies_with_id():
 
 
 def get_movies_with_people():
+    """
+    Get all the movies with the characters associated with it.
+    It returns the result as a dictionnary of characters indexed by film name
+    if the API returns a valid result. Otherwise it returns an empty dict.
+    """
+
     movies_by_id = get_movies_with_id()
     movies = {}
     if movies_by_id:
--- senndertest/senndermovies/views.py
+++ senndertest/senndermovies/views.py
@@ -3,6 +3,7 @@ from .processing import get_movies_with_people
 
 
 def movie_list(request):
+    """Renders all movies with the corresponding characters as a plain list"""
     context = {
         'movie_list': get_movies_with_people()
     }
